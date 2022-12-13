3490c7ecb4dba18db02b30fd1ab1d662e2c32fe8
KHAYO <jozeykhayo@gmail.com>
2022-12-13 02:56:07 +0200

    Adding Registration package and RegistrationController

--- ULR/ULR/src/main/java/com/example/ULR/appuser/AppUserService.java
+++ ULR/ULR/src/main/java/com/example/ULR/appuser/AppUserService.java
@@ -1,15 +1,21 @@
 package com.example.ULR.appuser;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+@AllArgsConstructor
 @Service
 public class AppUserService implements UserDetailsService {
-    private final
+    private final AppUserRepository appUserRepository;
+    private final static String USER_NOT_FOUND_MSG =
+            "user with email %s not found";
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
